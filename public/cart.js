d45c43d6fe42f25f24304d5f3c87a1f0a01f7f42
Juan Escalada <juanescalada175@gmail.com>
2022-05-19 15:48:35 -0700

    Polish cart and load contents dynamically

--- public/cart.js
+++ public/cart.js
@@ -8,10 +8,23 @@ async function loadPokemonById(pokemonId) {
 }
 
 async function loadPokemonToDOM(pokemonId, quantity) {
-    let pokemonData = await loadPokemonById(pokemonId)
-    
-    $("#results").append(content)
-    console.log("Total cost: " + parseFloat(pokemonData.price) * quantity)
+    let pokemon = await loadPokemonById(pokemonId)
+    let entry = `
+        <div class="row">
+            <div class="thumbnail-container">
+                <img src="${pokemon.sprite}" alt="${pokemon.name}" style="width:100%"
+                    onclick="location.href='pokemon.html?id=${pokemon.id}'" class="pokemon-image-thumb">
+            </div>
+            <div class="row pokemon-buy-details">
+                <h3 class="col card-price">${pokemon.name}</h3>
+                <h3 class="col card-price">$${pokemon.price}</h3>
+                <h3 class="col card-quantity" id="card-quantity-${pokemon.id}">Qty: ${quantity}</h3>
+                <h3 class="col card-quantity"> Total: $${pokemon.price * quantity}</h3>
+                <button class="col add-to-cart-button" onclick="removeFromCart(${pokemon.id})">Remove</button>
+            </div>
+        </div>
+    `
+    $("#cart").append(entry)
 }
 
 function loadCart() {
