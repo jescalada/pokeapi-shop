3a137c325d87a2afd1108de74425aea1a89e9863
Juan Escalada <juanescalada175@gmail.com>
2022-05-19 14:51:39 -0700

    Add cart loading function

---
+++ public/cart.js
@@ -0,0 +1,19 @@
+let userId = 1;
+
+function loadCart() {
+    let data = {
+        userId: userId,
+    }
+
+    fetch('/cart', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }).then(response => response.json()).then((data) => {
+        console.log(data);
+    });
+}
+
+loadCart();
